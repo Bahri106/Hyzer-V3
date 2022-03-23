3db1edc233f19a050a8e70499caf4989c441af0c
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-23 14:37:33 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,18 +1,17 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Minggu* :      *Rp 5.000*
-╠➥ *1 Bulan* :      *Rp 10.000*
-╠➥ *Permanen* : *Rp 25.000*
-╠➥ *Trial* :   *Rp 0*
+╠➥ *1 bulan* :      *Rp 15.000*
+╠➥ *4 bulan* :      *Rp 30.000*
+╠➥ *8 bulan* : *Rp 40.000*
 ║
 ╠═〘 𝐏𝐄𝐌𝐁𝐀𝐘𝐀𝐑𝐀𝐍 〙 ═
-╠➥ Topup/Pin
+╠➥ DANA/GOPAY
 ║
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Klil Button Di Bawah
 ║
-╠═〘 Hyzer 〙 ═`.trim(), '❖ Kanao-Bot', 'Gopay', '#viadigi', 'Dana', '#viaumobile', m)
+╠═〘 zifabotz 〙 ═`.trim(), '©️zifabotz', 'Gopay', '#viadigi', 'Dana', '#viaumobile', m)
 }
 
 handler.command = /^sewabot$/i
